3e97a047b1ef26035e771f7382df1068078d7b8d
slipstreamJumper <the.slipstream.jumper@gmail.com>
2023-09-14 23:13:56 -0400

    initial commit

---
+++ main2.py
@@ -0,0 +1,23 @@
+import myo
+
+class Listener(myo.DeviceListener):
+  def on_paired(self, event):
+    print("Hello, {}!".format(event.device_name))
+    event.device.vibrate(myo.VibrationType.short)
+  def on_unpaired(self, event):
+    return False  # Stop the hub
+  def on_orientation(self, event):
+    orientation = event.orientation
+    print(orientation)
+    acceleration = event.acceleration
+    print(acceleration)
+    gyroscope = event.gyroscope
+    print(gyroscope)
+    # ... do something with that
+
+if __name__ == '__main__':
+  myo.init(sdk_path='./myo-sdk-win-0.9.0/')
+  hub = myo.Hub()
+  listener = Listener()
+  while hub.run(listener.on_event, 500):
+    pass
