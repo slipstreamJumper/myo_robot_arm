55fac3721c222eff00c88564e9877fb26193116f
slipstreamJumper <the.slipstream.jumper@gmail.com>
2023-10-29 22:44:02 -0400

    initial commit

--- pyomyo_test.py
+++ pyomyo_test.py
@@ -14,8 +14,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
-upper = GPIO.PWM(26, 20)
-lower = GPIO.PWM(21, 20)
+upper = GPIO.PWM(26, 4)
+lower = GPIO.PWM(21, 4)
 
 #global vars
 gquat = 0
@@ -126,8 +126,8 @@ if __name__ == "__main__":
     p = multiprocessing.Process(target=worker, args=(q,))
     p.start()
 
-    upper.start(20)
-    lower.start(20)
+    upper.start(5)
+    lower.start(5)
 
 
 
